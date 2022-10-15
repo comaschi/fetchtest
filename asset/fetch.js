83fc87f49de84b8d81b3d2576d43abc69f9d0388
Diego <diego.comaschi@medhost.com>
2022-10-15 01:41:12 -0500

    randomuser working

--- asset/fetch.js
+++ asset/fetch.js
@@ -1,14 +1,56 @@
-let retrieve = () => {
-  let url = 'http://randomuser.me/api';
-  fetch(url)
+const eventB1 = document.getElementById('b1');
+
+const retrieve = async () => {
+  // let url = 'https://api.github.com/users/mralexgray/repos';
+  let url = 'https://randomuser.me/api';
+  
+  let jsonArray = await fetch(url)
     .then((response) => {
-      response.json();
-      console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Server response wasn't OK");
+      }
     })
     .then((data) => {
       console.log(data);
       console.log(data.results);
-    });
+      return data.results;
+    })
+    .catch((error) => console.log(error));
+
+  return jsonArray;
 };
 
-retrieve();
+// console.log(retrieve());
+
+eventB1.addEventListener('click', async () => {
+  const span = document.getElementById('ul1');
+  const results = await retrieve();
+
+  console.log(results);
+
+  for (const result of results) {
+    for (const key in result) {
+      console.log(`<li> ${result[key]} </li>`);
+
+      span.innerHTML += `<li> ${key} : ${result[key]} </li>`;
+      if (typeof result[key] === 'object') {
+        for (const key2 in result[key]) {
+          console.log(`<li> ${key}/${key2} : ${result[key][key2]} </li>`);
+          span.innerHTML += `<li> ${key}/${key2} : ${result[key][key2]} </li>`;
+          if (typeof result[key][key2] === 'object') {
+            for (const key3 in result[key][key2]) {
+              console.log(`<li> ${key}/${key2}/${key3} : ${result[key][key2][key3]} </li>`);
+              span.innerHTML += `<li> ${key}/${key2}/${key3} : ${result[key][key2][key3]} </li>`;
+            }
+          }
+
+
+        }
+      }
+    }
+  }
+});
+
+const forLoop ()
